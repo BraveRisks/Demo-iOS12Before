4476229b378f7b5ff32dfc625533b683a79a1655
Ray <S384861@sinyi.com.tw>
2020-01-31 09:08:44 +0800

    - create new project

---
+++ DemoiOS12BeforeTests/DemoiOS12BeforeTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoiOS12BeforeTests.swift
+//  DemoiOS12BeforeTests
+//
+//  Created by Ray on 2020/1/30.
+//  Copyright © 2020 Sinyi Realty Inc. All rights reserved.
+//
+
+import XCTest
+@testable import DemoiOS12Before
+
+class DemoiOS12BeforeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
